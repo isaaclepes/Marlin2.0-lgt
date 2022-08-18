ab7b851636ae6490818fc13b2423530adef270f9
isaaclepes <54421422+isaaclepes@users.noreply.github.com>
2022-08-18 16:41:51 -0600

    adjust variables

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -582,9 +582,9 @@
 
   #if ANY(LK5, LK5_PRO)
     // have dual blower. send "M303 E0 S200 C8" command to get PID.
-    #define DEFAULT_Kp 28.44
-    #define DEFAULT_Ki 2.41
-    #define DEFAULT_Kd 83.88
+    #define DEFAULT_Kp 40.61
+    #define DEFAULT_Ki 4.17
+    #define DEFAULT_Kd 98.97
   #else
     #define DEFAULT_Kp 17.22
     #define DEFAULT_Ki 1.00
@@ -663,9 +663,9 @@
   // 300x300 bed size
   #elif ANY(LK5, LK5_PRO)
     // send "M303 E-1 S80 C8" command to get PID.
-    #define DEFAULT_bedKp 174.64
-    #define DEFAULT_bedKi 27.33
-    #define DEFAULT_bedKd 743.95
+    #define DEFAULT_bedKp 153.33
+    #define DEFAULT_bedKi 29.71
+    #define DEFAULT_bedKd 527.57
   #elif ANY(U20, LK1, LK1_PRO)
     //From M303 command for Alfawise U20 :
     #define DEFAULT_bedKp 841.68
